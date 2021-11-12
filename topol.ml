90cb53bb227f881a6870908b038bd841c9ea91f9
Aleshkev <43296251+Aleshkev@users.noreply.github.com>
2021-11-12 23:51:36 +0100

    Refactor #1

--- topol.ml
+++ topol.ml
@@ -1,39 +1,28 @@
 exception Cykliczne
 
-let update key f map = map |> PMap.add key (f (map |> PMap.find key))
+let update key f default map =
+  map |> PMap.add key (f (try map |> PMap.find key with Not_found -> default))
 
 let topol dependencies =
-  (* Mapa z każdego wierzchołka na 0. *)
-  let zeroes =
-    List.fold_left
-      (fun t (b, aa) ->
-        List.fold_left (fun t a -> t |> PMap.add a 0) t aa |> PMap.add b 0)
-      PMap.empty dependencies
-  in
-
-  (* Nazwy wierzchołków. *)
-  let nodes = PMap.foldi (fun a _ t -> a :: t) zeroes [] in
-
   (* Graf to mapa z nazwy wierzchołka na listę nazw wierzchołków, do których
-     istnieje krawędź. *)
-  (* "Stopniem" nazywamy liczbę krawędzi wchodzących do wierzchołka. *)
-  let graph_with_no_edges = PMap.map (fun _ -> []) zeroes in
+     istnieje krawędź. "Stopniem" nazywamy liczbę krawędzi wchodzących do
+     wierzchołka. *)
   let graph, degrees =
     List.fold_left
-      (fun acc (b, aa) ->
+      (fun (graph, degrees) (b, aa) ->
+        let graph = graph |> update b Fun.id []
+        and degrees = degrees |> update b Fun.id 0 in
         List.fold_left
           (fun (graph, degrees) a ->
-            ( graph |> update a (fun bb -> b :: bb),
-              degrees |> update b (fun x -> x + 1) ))
-          acc aa)
-      (graph_with_no_edges, zeroes)
-      dependencies
+            let graph = graph |> update a (fun bb -> b :: bb) []
+            and degrees = degrees |> update b (fun x -> x + 1) 0 in
+            graph, degrees)
+          (graph, degrees) aa)
+      (PMap.empty, PMap.empty) dependencies
   in
 
-  (* Na kolejce na początku są wierzchołki, które na początku mają stopień 0. *)
-  let queue =
-    PMap.foldi (fun a degree t -> if degree = 0 then a :: t else t) degrees []
-  in
+  (* Wszystkie nazwy wierzchołków. *)
+  let nodes = PMap.foldi (fun a _ t -> a :: t) graph [] in
 
   (* [degrees] to aktualne stopnie wszystkich wierzchołków; [queue] to lista
      wierzchołków mających aktualnie stopień 0. Aktualny graf to [graph] bez
@@ -49,13 +38,20 @@ let topol dependencies =
            które po takim zmniejszeniu miałyby stopień 0. *)
         let queue', degrees' =
           List.fold_left
-            (fun (queue, degrees) b ->
-              ( (if degrees |> PMap.find b = 1 then b :: queue else queue),
-                degrees |> update b (fun x -> x - 1) ))
+            (fun (queue', degrees') b ->
+              let queue' =
+                if degrees' |> PMap.find b = 1 then b :: queue' else queue'
+              and degrees' = degrees' |> update b (fun x -> x - 1) 0 in
+              queue', degrees')
             (queue, degrees)
             (graph |> PMap.find a)
         in
         walk degrees' queue' (a :: acc)
+  in
+
+  (* Na kolejce na początku są wierzchołki, które na początku mają stopień 0. *)
+  let queue =
+    PMap.foldi (fun a degree t -> if degree = 0 then a :: t else t) degrees []
   in
 
   let result = walk degrees queue [] in
