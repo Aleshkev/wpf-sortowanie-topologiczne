988bedc606a9d580e667785d89dd7345b6c8ae56
Aleshkev <43296251+Aleshkev@users.noreply.github.com>
2021-12-01 17:11:56 +0100

    Imperative O(n log n), somewhat fast

--- topol.ml
+++ topol.ml
@@ -1,58 +1,58 @@
 exception Cykliczne
 
-(** Zamienia w [map] wartość dla [key] z jakiegoś [x] na [f x] jeżeli [key] jest
-    w [map], w przeciwnym wypadku wstawia [f default]. *)
-let update key f default map =
-  map |> PMap.add key (f (try map |> PMap.find key with Not_found -> default))
+open List
+
+let uniq = function
+  | [] -> []
+  | x :: t ->
+      fold_left (fun (x', r) x -> x, if x = x' then r else x :: r) (x, [ x ]) t
+      |> snd |> rev
+
+let find x arr =
+  let rec f lo hi =
+    if lo >= hi
+    then lo
+    else
+      let mid = (lo + hi) / 2 in
+      if arr.(mid) < x then f (mid + 1) hi else f lo mid
+  in
+  f 0 (Array.length arr)
+
+let fold_edges f acc v =
+  List.fold_left
+    (fun acc (y, row) -> fold_left (fun acc x -> f acc x y) acc row)
+    acc v
 
 let topol dependencies =
-  (* Graf to mapa z nazwy wierzchołka na listę nazw wierzchołków, do których
-     istnieje krawędź. "Stopniem" nazywamy liczbę krawędzi wchodzących do
-     wierzchołka. Update'ujemy z identycznością żeby na pewno były klucze dla
-     wszystkich wierzchołków z wejścia. *)
-  let graph, degrees =
-    List.fold_left
-      (fun (graph, degrees) (b, aa) ->
-        let graph = graph |> update b Fun.id []
-        and degrees = degrees |> update b Fun.id 0 in
-        List.fold_left
-          (fun (graph, degrees) a ->
-            ( graph |> update a (fun bb -> b :: bb) [],
-              degrees |> update a Fun.id 0 |> update b (fun x -> x + 1) 0 ))
-          (graph, degrees) aa)
-      (PMap.empty, PMap.empty) dependencies
-  in
+  let labels = dependencies |> fold_edges (fun acc a b -> a :: b :: acc) [] in
+  let labels = dependencies |> fold_left (fun acc (b, _) -> b :: acc) labels in
+  let labels = labels |> List.sort Stdlib.compare |> uniq in
+  let labels = Array.of_list labels in
+  let n_labels = Array.length labels in
+  let index a = labels |> find a in
 
-  (* Wszystkie nazwy wierzchołków. *)
-  let nodes = PMap.foldi (fun a _ t -> a :: t) graph [] in
+  let degrees = Array.make n_labels 0 in
+  let graph = Array.make n_labels [] in
+  fold_edges
+    (fun () a b ->
+      let a' = index a and b' = index b in
+      degrees.(b') <- degrees.(b') + 1;
+      graph.(a') <- b' :: graph.(a');
+      ())
+    () dependencies;
 
-  (* [degrees] to aktualne stopnie wszystkich wierzchołków; [queue] to lista
-     wierzchołków mających aktualnie stopień 0. Aktualny graf to [graph] bez
-     wierzchołków, które są w [queue]. Wynik procedury to wierzchołki
-     z aktualnego grafu, posortowane topologicznie, całość złączona z [acc].
-     Chyba, że istnieje cykl, wtedy wynikiem jest jakiś mniejszy podzbiór tych
-     wierzchołków. *)
-  let rec walk degrees queue acc =
-    match queue with
-    | [] -> acc
-    | a :: queue ->
-        (* Usuwamy krawędzie wychodzące z [a]. Dodajemy do kolejki wierzchołki,
-           które po takim zmniejszeniu miałyby stopień 0. *)
-        let queue', degrees' =
-          List.fold_left
-            (fun (queue, degrees) b ->
-              ( (if degrees |> PMap.find b = 1 then b :: queue else queue),
-                degrees |> update b (fun x -> x - 1) 0 ))
-            (queue, degrees)
-            (graph |> PMap.find a)
-        in
-        walk degrees' queue' (a :: acc)
-  in
+  let queue = ref [] in
+  Array.iteri (fun i x -> if x = 0 then queue := i :: !queue) degrees;
 
-  (* Na kolejce na początku są wierzchołki, które na początku mają stopień 0. *)
-  let queue =
-    PMap.foldi (fun a degree t -> if degree = 0 then a :: t else t) degrees []
-  in
-
-  let result = walk degrees queue [] in
-  if List.length result < List.length nodes then raise Cykliczne else result
+  let result = ref [] in
+  while !queue <> [] do
+    let a = hd !queue in
+    result := labels.(a) :: !result;
+    queue := tl !queue;
+    iter
+      (fun b ->
+        degrees.(b) <- degrees.(b) - 1;
+        if degrees.(b) = 0 then queue := b :: !queue)
+      graph.(a)
+  done;
+  if length !result < n_labels then raise Cykliczne else !result
