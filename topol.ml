bff64c8c7d4acced5ff3734ac61995d1f097cc35
Aleshkev <43296251+Aleshkev@users.noreply.github.com>
2021-12-01 18:15:14 +0100

    O(n), imperative

--- topol.ml
+++ topol.ml
@@ -1,58 +1,63 @@
-exception Cykliczne
-
 open List
 
-let uniq = function
-  | [] -> []
-  | x :: t ->
-      fold_left (fun (x', r) x -> x, if x = x' then r else x :: r) (x, [ x ]) t
-      |> snd |> rev
+exception Cykliczne
 
-let find x arr =
-  let rec f lo hi =
-    if lo >= hi
-    then lo
-    else
-      let mid = (lo + hi) / 2 in
-      if arr.(mid) < x then f (mid + 1) hi else f lo mid
-  in
-  f 0 (Array.length arr)
+let topol dep =
+  if dep = []
+  then []
+  else
+    (* Wywołuje [f a b] na każdym [a b] że [a] ma być przed [b] w wyniku. *)
+    let iter_edges f =
+      dep |> iter (fun (y, row) -> iter (fun x -> f x y) row)
+    in
 
-let fold_edges f acc v =
-  List.fold_left
-    (fun acc (y, row) -> fold_left (fun acc x -> f acc x y) acc row)
-    acc v
+    (* Przypisujemy każdemu wierzchołkowi liczbę całkowitą. *)
+    let ids = Hashtbl.create (length dep) in
+    let next_id = ref 0 in
+    let new_label x =
+      if Option.is_none @@ Hashtbl.find_opt ids x
+      then (
+        Hashtbl.add ids x !next_id;
+        incr next_id)
+    in
+    iter_edges (fun a b ->
+        new_label a;
+        new_label b);
+    dep |> iter (fun (b, _) -> new_label b);
+    let id_of a = Hashtbl.find ids a in
 
-let topol dependencies =
-  let labels = dependencies |> fold_edges (fun acc a b -> a :: b :: acc) [] in
-  let labels = dependencies |> fold_left (fun acc (b, _) -> b :: acc) labels in
-  let labels = labels |> List.sort Stdlib.compare |> uniq in
-  let labels = Array.of_list labels in
-  let n_labels = Array.length labels in
-  let index a = labels |> find a in
+    (* Tworzymy tablicę żeby dało się tę zamianę odwrócić. *)
+    let n_nodes = Hashtbl.length ids in
+    let labels = Array.make n_nodes (fst @@ hd dep) in
+    ids |> Hashtbl.iter (fun k v -> labels.(v) <- k);
+    let label_of a = labels.(a) in
 
-  let degrees = Array.make n_labels 0 in
-  let graph = Array.make n_labels [] in
-  fold_edges
-    (fun () a b ->
-      let a' = index a and b' = index b in
-      degrees.(b') <- degrees.(b') + 1;
-      graph.(a') <- b' :: graph.(a');
-      ())
-    () dependencies;
+    (* "Stopniem" nazywamy liczbę krawędzi wchodzących do wierzchołka. *)
+    let degrees = Array.make n_nodes 0 in
+    (* [graph.(a)] to lista wierzchołków, do których jest krawędź z [a]. *)
+    let graph = Array.make n_nodes [] in
+    iter_edges (fun a b ->
+        let a' = id_of a and b' = id_of b in
+        degrees.(b') <- degrees.(b') + 1;
+        graph.(a') <- b' :: graph.(a'));
 
-  let queue = ref [] in
-  Array.iteri (fun i x -> if x = 0 then queue := i :: !queue) degrees;
+    (* Na kolejce zawsze będą wszystkie wierzchołki o "stopniu" 0. *)
+    let queue = ref [] in
+    degrees |> Array.iteri (fun i x -> if x = 0 then queue := i :: !queue);
 
-  let result = ref [] in
-  while !queue <> [] do
-    let a = hd !queue in
-    result := labels.(a) :: !result;
-    queue := tl !queue;
-    iter
-      (fun b ->
-        degrees.(b) <- degrees.(b) - 1;
-        if degrees.(b) = 0 then queue := b :: !queue)
+    let result = ref [] in
+    while !queue <> [] do
+      let a = hd !queue in
+      result := a :: !result;
+      queue := tl !queue;
+
       graph.(a)
-  done;
-  if length !result < n_labels then raise Cykliczne else !result
+      |> iter (fun b ->
+             degrees.(b) <- degrees.(b) - 1;
+             if degrees.(b) = 0 then queue := b :: !queue)
+    done;
+
+    if length !result < n_nodes
+    then (* Nie przeszliśmy wszystkich wierzchołków. *)
+      raise Cykliczne
+    else map label_of !result
